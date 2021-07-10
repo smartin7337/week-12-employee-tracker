2e691a46e8c9069380917744d7b37ef751efbf94
Vossie <sv7337@outlook.com>
2021-07-10 17:33:08 -0400

    building the sql table

---
+++ db.sql
@@ -0,0 +1,30 @@
+
+DROP DATABASE IF EXISTS employeetracker;
+CREATE DATABASE employeetracker;
+USE employeetracker;
+
+CREATE TABLE department (
+   id int NOT NULL AUTO_INCREMENT,
+   name VARCHAR(30) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+   id int NOT NULL AUTO_INCREMENT,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL NOT NULL,
+   department_id int NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+   id int NOT NULL AUTO_INCREMENT,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id int NOT NULL,
+   manager_id int,
+   PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);

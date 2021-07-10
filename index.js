d5e770daafa54463fc7f77513c64bd97fc3f6fe7
Vossie <sv7337@outlook.com>
2021-07-10 16:44:19 -0400

    started the questions/options

--- index.js
+++ index.js
@@ -7,3 +7,42 @@ var connection = mysql.createConnection({
 
 const inquirer = require("inquirer");
 let responses = [];
+
+const questions = [
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        { name: "Add a department", value: "addDepartment" },
+        { name: "Add a role", value: "addRole" },
+        { name: "Add an employee", value: "addEmployee" },
+        { name: "View departments", value: "viewDepartments" },
+        { name: "View roles", value: "viewRoles" },
+        { name: "View employees", value: "viewEmployees" },
+        { name: "Update employee roles", value: "updateRoles" },
+      ],
+    },
+    {
+      type: "input",
+      name: "deptName",
+      message: "What name?",
+      when(answers) {
+        return answers.choice == "addDepartment";
+      },
+    },
+    {
+      type: "input",
+      name: "roleName",
+      message: "What name?",
+      when(answers) {
+        return answers.choice == "addRole";
+      },
+    },
+    {
+      type: "list",
+      name: "roleDept",
+      message: "What department?",
+      choices: function () 
+    }
+    

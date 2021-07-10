a9227e6e1bf33c3bffd78dace2726d2e716be6f2
Vossie <sv7337@outlook.com>
2021-07-10 17:14:19 -0400

    set roles/employee function  and the inquirer prompt if questions

--- index.js
+++ index.js
@@ -45,4 +45,89 @@ const questions = [
       message: "What department?",
       choices: function () 
     }
-    
+    var c = [{}];
+      connection.query(
+        "SELECT * FROM `department`;",
+        function (error, results, fields) {
+          if (error) throw error;
+          c = results;
+        }
+      );
+      return c;
+    },
+    when(answers) {
+      return answers.choice == "addRole";
+    },
+  },
+];
+const confirm = [
+  {
+    type: "confirm",
+    name: "askAgain",
+    message: "Would you like to do something else?",
+    default: true,
+  },
+];
+
+function addDepartment(name) {
+  connection.query(
+    'INSERT INTO `department` (`name`) VALUES ("' + name + '");',
+    function (error, results, fields) {
+      if (error) throw error;
+    }
+  );
+}
+  function viewDepartments() {
+    connection.query(
+      "SELECT * FROM `department`;",
+      function (error, results, fields) {
+        if (error) throw error;
+        console.table(results);
+      }
+    );
+  }
+  function viewRoles() {
+    connection.query("SELECT * FROM `role`;", function (error, results, fields) {
+      if (error) throw error;
+      console.table(results);
+    });
+  }
+  function viewEmployees() {
+    connection.query(
+      "SELECT * FROM `employee`;",
+      function (error, results, fields) {
+        if (error) throw error;
+        console.table(results);
+      }
+    );
+  }
+  
+  function ask() {
+    inquirer.prompt(questions).then((answers) => {
+      //  model for things working:
+      if (answers.choice == "addDepartment") {
+        addDepartment(answers.deptName);
+      }
+      if (answers.choice == "viewDepartments") {
+        viewDepartments();
+      }
+  
+      if (answers.choice == "viewRoles") {
+        viewRoles();
+      }
+      if (answers.choice == "viewEmployees") {
+        viewEmployees();
+      }
+  
+  // creates a function to initialize app
+function init() {
+    ask();
+  }
+  
+  // call to initialize app
+  init();
+  
+  //export
+  module.exports = {
+    init,
+  };
